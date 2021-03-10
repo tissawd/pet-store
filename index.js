6295a33a4265d886e0b945f1b64a38319e0cf9dd
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-03-10 16:28:36 -0600

    Pending pets completed

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var fs = require('fs');
 var fetch = require('node-fetch');
 
-var getAvailPetsAsync = function(){
-    var pets = fetch('https://petstore.swagger.io/v2/pet/findByStatus?status=available')
+var getPetsAsync = function(status){
+    var filter = status || 'available';
+    var pets = fetch(`https://petstore.swagger.io/v2/pet/findByStatus?status=${filter}`)
     .then(response => response.json())
     .then(data => data.map(x => x.name))
     return pets;
 }
 
-getAvailPetsAsync().then(result => fs.writeFile('available-pets.txt', result.join('\n'), function(err){
+getPetsAsync('available').then(result => fs.writeFile('available-pets.txt', result.join('\n'), function(err){
     if(err) return console.log(err);
 }));
 
 
+getPetsAsync('pending').then(result => fs.writeFile('pending-pets.txt', result.join('\n'), function(err){
+  if(err) return console.log(err);
+}));
