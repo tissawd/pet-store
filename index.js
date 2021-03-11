f015845e839a5cbe18b4183ccf9fd34bc75d207a
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-03-10 19:43:24 -0600

    Working on stretch feature to add pet from text file with command line arg path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var fetch = require('node-fetch');
+const { resolve } = require('path');
 
 var getPetsAsync = function(status){
     var filter = status || 'available';
@@ -17,3 +18,29 @@ getPetsAsync('available').then(result => fs.writeFile('available-pets.txt', resu
 getPetsAsync('pending').then(result => fs.writeFile('pending-pets.txt', result.join('\n'), function(err){
   if(err) return console.log(err);
 }));
+
+
+let pathToNewPet = process.argv[2];
+
+if (pathToNewPet){
+  fs.readFile(pathToNewPet, 'utf8', (err, data) => {
+    if (err){
+      return err;
+    } else{
+      addNewPetToStore(data);
+    }
+  })
+}
+
+
+ function addNewPetToStore(pet){
+   fetch("https://petstore.swagger.io/v2/pet", {
+     method: "POST",
+     body: pet
+   }).then(response => {
+     console.log(response);
+     console.log('Pet added');
+   })
+}
+
+
